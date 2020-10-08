efdd1437be7b0d5ba9e050fe4056ebae5a78ac84
Joaquin Gayoso-Cabada <gayoxo@gmail.com>
2020-10-08 20:18:24 +0200

    Hay que crear mas exports

---
+++ ClavileñoXLSExport/src/fdi/ucm/server/exportparser/xls/v2/NameConstantsXLS.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package fdi.ucm.server.exportparser.xls.v2;
+
+/**
+ * Clase que implementa los estaticos de nombre para el SQL
+ * @author Joaquin Gayoso-Cabada
+ *
+ */
+public class NameConstantsXLS {
+	//META
+	public static final String META = "Meta";
+	public static final String TYPE = "Type";
+	public static final String IGNORED = "IGNORED";
+		
+}
